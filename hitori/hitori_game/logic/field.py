84d6db46fc827e019e972b5639ed53de29d14600
trrail <55761609+trrail@users.noreply.github.com>
2021-01-06 18:08:25 +0300

    Add files via upload

---
+++ hitori/hitori_game/logic/field.py
@@ -0,0 +1,26 @@
+from hitori_game.logic.const import Color
+
+
+class Field:
+    def __init__(self, color: Color, number: int):
+        self.color = color
+        self.number = number
+        self.have_near_another_field = False
+
+    def change_color(self):
+        colors = {Color.BLACK: self.set_grey,
+                  Color.WHITE: self.set_black,
+                  Color.GREY: self.set_white}
+        colors[self.color]()
+
+    def set_black(self):
+        self.color = Color.BLACK
+
+    def set_white(self):
+        self.color = Color.WHITE
+
+    def set_grey(self):
+        self.color = Color.GREY
+
+    def set_red(self):
+        self.color = Color.RED
---
+++ hitori/hitori_game/main.py
@@ -0,0 +1,6 @@
+from hitori_game.gui.window import Window
+
+
+if __name__ == '__main__':
+    window = Window()
+    window.start()
